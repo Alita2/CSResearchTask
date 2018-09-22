849f552f2777333a647c5c261a411d13d608d187
Alita2 <h1410121@nushigh.edu.sg>
2018-09-22 13:42:14 +0800

    edit

--- CycleSortPane.java
+++ CycleSortPane.java
@@ -193,7 +193,7 @@ public class CycleSortPane extends Pane {
                     explain1.setText("The number of numbers here <= "+array[smaller2[0]]+" is: "+(smaller2[0]-b));
                     explain1.setWrappingWidth(110);
                     explain1.setX(Math.floor(width/n)*(((double) b)+((double)(n-b+1))/2.0)-60);
-                    explain2.setText("Swap values with the element at index start+smaller = "+""+b+"+"+(smaller2[0])+"="+(smaller2[0]+b));
+                    explain2.setText("Swap values with the element at index start+smaller = "+""+b+"+"+(smaller2[0]-b)+"="+(smaller2[0]));
                     explain2.setY(height/2-102);
                     explain2.setX(0);
                 });
@@ -264,7 +264,7 @@ public class CycleSortPane extends Pane {
                     explain1.setText("The number of numbers here <= "+array[smaller2[0]]+" is: "+(smaller2[0]-b));
                     explain1.setWrappingWidth(120);
                     explain1.setX(Math.floor(width/n)*(((double) b)+((double)(n-b+1))/2.0)-60);
-                    explain2.setText("Swap values with the element at index start+smaller = "+""+b+"+"+(smaller2[0])+"="+(smaller2[0]+b));
+                    explain2.setText("Swap values with the element at index start+smaller = "+""+b+"+"+(smaller2[0]-b)+"="+(smaller2[0]));
                     explain2.setY(height/2-102);
                     explain2.setX(0);
                 });
@@ -333,7 +333,7 @@ public class CycleSortPane extends Pane {
                         explain1.setText("The number of numbers here <= "+array[smaller2[0]]+" is: "+(smaller2[0]-b));
                         explain1.setWrappingWidth(120);
                         explain1.setX(Math.floor(width/n)*(((double) b)+((double)(n-b+1))/2.0)-60);
-                        explain2.setText("Swap values with the element at index start+smaller = "+""+b+"+"+(smaller2[0])+"="+(smaller2[0]+b));
+                        explain2.setText("Swap values with the element at index start+smaller = "+""+b+"+"+(smaller2[0]-b)+"="+(smaller2[0]));
                         explain2.setY(height/2-102);
                         explain2.setX(0);
                     });
